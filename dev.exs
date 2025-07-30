4542676167c0ab4369ed1fbc275325fd99a5df87
Jonatan Männchen <jonatan@maennchen.ch>
2025-07-30 11:19:41 +0200

    Live Reload Dev Setup

--- dev.exs
+++ dev.exs
@@ -5,7 +5,19 @@ Application.put_env(:ash_atlas, DemoWeb.Endpoint,
   http: [port: System.get_env("PORT", "4000")],
   debug_errors: true,
   check_origin: false,
-  pubsub_server: Demo.PubSub
+  pubsub_server: Demo.PubSub,
+  watchers: [
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
+  ],
+  live_reload: [
+    patterns: [
+      ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
+      ~r"priv/gettext/.*(po)$",
+      ~r"lib/ash_atlas/(live|views|pages|components)/.*(ex)$",
+      ~r"lib/ash_atlas/templates/.*(eex)$"
+    ]
+  ]
 )
 
 defmodule DemoWeb.Router do
