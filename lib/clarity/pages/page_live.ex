31289ab7fe0b75767e60c49c49f76bc6fb532de1
Jonatan Männchen <jonatan@maennchen.ch>
2025-10-28 13:29:04 +0100

    Async Fixes

--- lib/clarity/pages/page_live.ex
+++ lib/clarity/pages/page_live.ex
@@ -19,29 +19,20 @@ defmodule Clarity.PageLive do
       :timer.send_interval(100, :refresh)
     end
 
-    clarity = Clarity.get(socket.assigns.clarity_pid, :partial)
-    update_count = Graph.get_update_count(clarity.graph)
+    navigation_fun = if connected?(socket), do: &push_patch/2, else: &push_navigate/2
 
     socket =
-      assign(socket,
-        clarity: clarity,
-        update_count: update_count,
+      socket
+      |> assign(
         zoom_level: {1, 1},
         show_navigation: false,
         data: AsyncResult.loading(),
-        lens: nil,
-        vertex: nil,
-        content: nil,
-        contents: [],
-        page_title: "Loading...",
-        breadcrumbs: []
+        page_title: "Loading..."
       )
+      |> fetch_clarity()
+      |> handle_routing(params, navigation_fun)
 
-    if connected?(socket) do
-      {:ok, handle_routing(socket, params, &push_patch/2)}
-    else
-      {:ok, socket}
-    end
+    {:ok, socket}
   end
 
   @impl Phoenix.LiveView
@@ -51,19 +42,15 @@ defmodule Clarity.PageLive do
 
   @impl Phoenix.LiveView
   def handle_async(:data, {:ok, %{data: new_data}}, socket) do
-    old_data = socket.assigns[:data]
-
-    if old_data && old_data.ok? do
-      Graph.delete(old_data.result.graph)
-      Graph.delete(old_data.result.zoom_graph)
+    if socket.assigns.data.ok? do
+      Graph.delete(socket.assigns.data.result.graph)
+      Graph.delete(socket.assigns.data.result.zoom_graph)
     end
 
-    {:noreply, assign(socket, data: AsyncResult.ok(socket.assigns.data, new_data))}
+    {:noreply, assign(socket, data: AsyncResult.ok(new_data))}
   end
 
-  def handle_async(:data, {:exit, reason}, socket) do
-    {:noreply, assign(socket, data: AsyncResult.failed(socket.assigns.data, {:exit, reason}))}
-  end
+  def handle_async(:data, {:exit, reason}, socket), do: {:noreply, data_error(socket, reason)}
 
   @spec handle_routing(Socket.t(), map(), (Socket.t(), keyword() -> Socket.t())) :: Socket.t()
   defp handle_routing(socket, params, navigate_fn) do
@@ -87,13 +74,18 @@ defmodule Clarity.PageLive do
   @spec handle_root_route(Socket.t(), (Socket.t(), keyword() -> Socket.t())) :: Socket.t()
   defp handle_root_route(socket, navigate_fn) do
     default_lens_id = Clarity.Config.fetch_default_perspective_lens!()
-    navigate_fn.(socket, to: Path.join([socket.assigns.prefix, default_lens_id]))
+
+    socket
+    |> assign(:data, AsyncResult.loading())
+    |> navigate_fn.(to: Path.join([socket.assigns.prefix, default_lens_id]))
   end
 
   @spec handle_lens_route(String.t(), Socket.t(), (Socket.t(), keyword() -> Socket.t())) ::
           Socket.t()
   defp handle_lens_route(lens_id, socket, navigate_fn) do
-    navigate_fn.(socket, to: Path.join([socket.assigns.prefix, lens_id, "root"]))
+    socket
+    |> assign(:data, AsyncResult.loading())
+    |> navigate_fn.(to: Path.join([socket.assigns.prefix, lens_id, "root"]))
   end
 
   @spec handle_vertex_route(String.t(), String.t(), Socket.t(), navigation_fn) :: Socket.t()
@@ -117,60 +109,88 @@ defmodule Clarity.PageLive do
 
   @spec handle_page_route(String.t(), String.t(), String.t(), Socket.t()) :: Socket.t()
   defp handle_page_route(lens_id, vertex_id, content_id, socket) do
+    socket = fetch_clarity(socket)
+
+    socket =
+      with {:ok, socket} <- fetch_lens(socket, lens_id),
+           {:ok, socket} <- fetch_vertex(socket, vertex_id, content_id) do
+        socket
+      else
+        {:error, socket, reason} ->
+          data_error(socket, reason)
+      end
+
+    socket
+    |> load_data_async()
+    |> update_page_title()
+  end
+
+  @spec fetch_clarity(Socket.t()) :: Socket.t()
+  defp fetch_clarity(socket) do
     clarity = Clarity.get(socket.assigns.clarity_pid, :partial)
     update_count = Graph.get_update_count(clarity.graph)
 
-    socket = assign(socket, clarity: clarity, update_count: update_count, show_navigation: false)
+    assign(socket, clarity: clarity, update_count: update_count)
+  end
 
+  @spec fetch_lens(Socket.t(), String.t()) ::
+          {:ok, Socket.t()} | {:error, Socket.t(), :lens_not_found}
+  defp fetch_lens(socket, lens_id) do
     case Lensmaker.get_lens_by_id(lens_id) do
-      {:error, :lens_not_found} ->
-        assign(socket,
-          lens: nil,
-          vertex: nil,
-          content: nil,
-          contents: [],
-          page_title: "Lens Not Found",
-          data: AsyncResult.failed(socket.assigns.data, :lens_not_found)
-        )
-
       {:ok, lens} ->
-        case Graph.get_vertex(clarity.graph, vertex_id) do
-          nil ->
-            assign(socket,
-              lens: lens,
-              vertex: nil,
-              content: nil,
-              contents: [],
-              page_title: "Vertex Not Found",
-              breadcrumbs: [],
-              data: AsyncResult.failed(socket.assigns.data, :vertex_not_found)
-            )
+        {:ok, assign(socket, lens: lens)}
 
-          vertex ->
-            contents = Content.get_contents_for_vertex(vertex, lens)
-
-            breadcrumbs = Graph.breadcrumbs(clarity.graph, vertex) || [vertex]
-
-            content = Enum.find(contents, &(&1.id == content_id))
-
-            socket =
-              socket
-              |> assign(
-                lens: lens,
-                vertex: vertex,
-                contents: contents,
-                content: content,
-                breadcrumbs: breadcrumbs
-              )
-              |> update_page_title()
-              |> load_data_async()
-
-            socket
-        end
+      {:error, :lens_not_found} ->
+        {:error, assign(socket, lens: nil, vertex: nil, content: nil, contents: []),
+         :lens_not_found}
     end
   end
 
+  @spec fetch_vertex(Socket.t(), String.t(), String.t()) ::
+          {:ok, Socket.t()} | {:error, Socket.t(), :vertex_not_found}
+  defp fetch_vertex(socket, vertex_id, content_id) do
+    case Graph.get_vertex(socket.assigns.clarity.graph, vertex_id) do
+      nil ->
+        {:error,
+         assign(socket,
+           vertex: nil,
+           content: nil,
+           contents: []
+         ), :vertex_not_found}
+
+      vertex ->
+        contents = Content.get_contents_for_vertex(vertex, socket.assigns.lens)
+        content = Enum.find(contents, &(&1.id == content_id))
+
+        breadcrumbs = Graph.breadcrumbs(socket.assigns.clarity.graph, vertex) || [vertex]
+
+        {:ok,
+         assign(socket,
+           vertex: vertex,
+           contents: contents,
+           content: content,
+           breadcrumbs: breadcrumbs
+         )}
+    end
+  end
+
+  @spec data_error(Socket.t(), term()) :: Socket.t()
+  defp data_error(socket, reason) do
+    if socket.assigns.data.ok? do
+      Graph.delete(socket.assigns.data.result.graph)
+      Graph.delete(socket.assigns.data.result.zoom_graph)
+    end
+
+    assign(socket, data: AsyncResult.failed(AsyncResult.loading(), reason))
+  end
+
   @spec load_data_async(Socket.t()) :: Socket.t()
+  defp load_data_async(socket)
+
+  defp load_data_async(%Socket{assigns: %{data: %AsyncResult{failed: reason}}} = socket)
+       when reason != nil,
+       do: socket
+
   defp load_data_async(socket) do
     %{
       clarity: clarity,
@@ -261,13 +281,10 @@ defmodule Clarity.PageLive do
           |> assign(clarity: clarity, update_count: new_update_count)
           |> load_data_async()
 
-        socket.assigns.data.failed? ->
-          socket
-          |> assign(clarity: clarity, update_count: new_update_count)
-          |> handle_routing(socket.assigns.params, fn socket, _opts -> socket end)
-
         true ->
           socket
+          |> assign(clarity: clarity, update_count: new_update_count)
+          |> handle_routing(socket.assigns.params, &push_patch/2)
       end
 
     {:noreply, socket}
@@ -282,6 +299,14 @@ defmodule Clarity.PageLive do
   end
 
   @spec update_page_title(Socket.t()) :: Socket.t()
+  defp update_page_title(socket)
+
+  defp update_page_title(%Socket{assigns: %{lens: nil}} = socket),
+    do: assign(socket, page_title: "Lens Not Found")
+
+  defp update_page_title(%Socket{assigns: %{vertex: nil}} = socket),
+    do: assign(socket, page_title: "Vertex Not Found")
+
   defp update_page_title(socket) do
     page_title =
       socket.assigns.breadcrumbs
